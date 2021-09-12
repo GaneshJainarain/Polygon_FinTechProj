b8e06265c0d017820b6575b163eada696ea6cb72
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-09-12 03:08:40 -0400

    Init repo connected to Polygon API

--- main/main.py
+++ main/main.py
@@ -1,14 +1,15 @@
 from polygon import RESTClient
+import config
 
 
 def main():
-    key = "0IdAc21OLMPwfoOUBjJeYuo3dlbhI6TK"
+    key = config.api_key
 
     # RESTClient can be used as a context manager to facilitate closing the underlying http session
     # https://requests.readthedocs.io/en/master/user/advanced/#session-objects
     with RESTClient(key) as client:
-        resp = client.stocks_equities_daily_open_close("AAPL", "2021-06-11")
-        print(f"On: {resp.from_} Apple opened at {resp.open} and closed at {resp.close}")
+        resp = client.stocks_equities_daily_open_close("AMD", "2021-06-11")
+        print(f"On: {resp.from_} AMD opened at {resp.open} and closed at {resp.close}")
 
 
 if __name__ == '__main__':
